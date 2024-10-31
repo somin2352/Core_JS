855bfd0ca6ea10de450c9cf0fb2017171604501d
somin2352 <somin030502@gmail.com>
2024-10-31 16:16:08 +0900

    조건문

---
+++ client/chapter/core/09-1.condition.js
@@ -0,0 +1,55 @@
+/* ---------------- */
+/* Condition        */
+/* ---------------- */
+
+// const answer = prompt("자바스크립트의 '공식' 이름은 무엇일까요?");
+// if (answer === 'ECMAScript') {
+//   alert('정답입니다!');
+// } else {
+//   alert('모르셨나요? 정답은 ECMAScript입니다!');
+// }
+
+// const num = prompt('숫자를 입력하세요.', 0);
+// if (num > 0) {
+//   alert(1);
+// } else if (num < 0) {
+//   alert(-1);
+// } else if (num == 0) {
+//   alert(0);
+// }
+
+// let a,b;
+// let result = a + b < 4 ? '미만' : '이상';
+
+let login;
+let message =
+  login == '직원'
+    ? '안녕하세요.'
+    : login == '임원'
+      ? '환영합니다.'
+      : login == ''
+        ? '로그인이 필요합니다.'
+        : '';
+
+// 그 영화 봤니?
+//     ↓
+// Yes | No
+//     | 영화 볼거니?
+//           ↓
+//       Yes | No
+
+// 영화 봤니?
+let didWatchMovie;
+
+// 영화 볼거니?
+let goingToWatchMovie;
+
+// if 문(statement)
+
+// else 절(clause)
+
+// else if 복수 조건 처리
+
+// 조건부 연산자
+
+// 멀티 조건부 연산자 식
