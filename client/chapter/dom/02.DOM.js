fa9c38cbcd034c376142c6207026db249d05210c
somin2352 <somin030502@gmail.com>
2024-11-14 14:34:21 +0900

    querySelector 등 노드 요소 찾기

---
+++ client/chapter/dom/02.DOM.js
@@ -0,0 +1,50 @@
+/* --------------------------------- */
+/* DOM traversal                     */
+/* --------------------------------- */
+
+/* 모든 노드에서 사용 */
+// - parentNode
+// - childNodes
+// - firstChild
+// - lastChild
+// - previousSibling
+// - nextSibling
+
+/* 요소 노드에서만 사용 가능 */
+// - parentElement
+// - children
+// - firstElementChild
+// - lastElementChild
+// - previousElementSibling
+// - nextElementSibling
+
+// const second = document.getElementsByClassName('second');
+// const _second = document.querySelector('.second');
+
+// console.log(second.parentElement); //h1
+// console.log(second.parentElementSibling); //span.first
+// console.log(second.nextElementSibling); //buton
+// console.log(second.children);
+
+// 1. nav 태그 요소
+const nav = document.querySelectorAll('nav');
+
+// 2. nav 태그 안에 잇는 about li 태그 요소
+const about = nav.querySelector('.about');
+
+// 3. data-name이 contact인 li 태그 요소
+const contact = nav.querySelector('li[data-name="contact"]');
+// 4. nav 요소 안에 있는 모든 자식 요소
+const children = nav.querySelectorAll('*');
+
+/* 문서 대상 찾기 */
+// - getElementById
+// - getElementsByTagName
+// - getElementsByClassName
+// - querySelector
+// - querySelectorAll
+// - closest
+
+/* 문서 대상 확인 */
+// - matches
+// - contains
