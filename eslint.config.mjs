b2c3d32822b78b66df6243119ca07b00b2a63e1f
somin2352 <somin030502@gmail.com>
2024-11-14 15:44:46 +0900

    type.js의 함수들 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,16 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 
+const types = {
+  isObject: true,
+  isArray: true,
+  isString: true,
+  isNumber: true,
+  isNull: true,
+  isUndefined: true,
+  isFunction: true,
+};
+
 export default [
   pluginJs.configs.recommended,
   {
