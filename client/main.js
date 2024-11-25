f0de28efb3e9ce7faa45f8527861724f08fdef6c
somin2352 <somin030502@gmail.com>
2024-11-25 16:28:20 +0900

    로컬 스토리지 함수를 활용한 input value 저장 함수

--- client/main.js
+++ client/main.js
@@ -1,79 +1,32 @@
-import {
-  insertLast,
-  som,
-  getNode,
-  renderUserCard,
-  changeColor,
-  renderLoadingSpinner,
-  delayP,
-  renderEmptyCard,
-} from './lib/index.js';
+import { getStorage, getNode, setStorage, deleteStorage } from './lib/index.js';
 
-const END_POINT = 'https://jsonplaceholder.typicode.com/users';
+// 1. input event binding
+// 2. input 값을 로컬 스토리지에 저장(타이핑하는 순간순간마다)
+// 3. init 함수 안에서 로컬스토리지에 있는 값을 가져와 input의 value로 설정
+// 4. 새로고침 -> 데이터 유지
 
-const userCardInner = getNode('.user-card-inner');
+const text = getNode('textarea');
+const clear = getNode('button[data-name="clear"]');
 
-// user 데이터 fetch
-async function renderUserList() {
-  // 데이터 호출 전에 스피너를 보여줌
-  renderLoadingSpinner(userCardInner);
-
-  try {
-    const response = await som.get(END_POINT);
-
-    // 데이터 불러오기를 완료하면 로딩 스피너 제거
-    // getNode('.loadingSpinner').remove();
-
-    // 로딩 스피너를 부드럽게 사라지게 하기 위한 애니메이션
-    gsap.to('.loadingSpinner', {
-      opacity: 0,
-      onComplete() {
-        this._targets[0].remove();
-      },
-    });
-
-    const data = response.data;
-
-    // delay를 주어서 아래 코드의 렌더링을 늦춤
-    await delayP(500);
-
-    // fetch 데이터 유저의 이름만 콘솔에 출력
-    data.forEach((user) => renderUserCard(userCardInner, user));
-
-    // 카드 컴포넌트에 색상 주기
-    changeColor('.user-card');
-
-    // 카드 컴포넌트 렌더링 애니메이션
-    gsap.from('.user-card', {
-      x: -100,
-      opacity: 0,
-      stagger: 0.1,
-    });
-  } catch {
-    // 실제 에러가 발생 후 empty 카드가 렌더링
-    renderEmptyCard(userCardInner);
-    throw new Error('error@');
-  }
-}
-
-renderUserList();
-
-// delete 이벤트
-// userCardInner에 이벤트를 주고 이벤트 위임
-function handleDeleteCard(e) {
-  const button = e.target.closest('button');
-
-  if (!button) return;
-
-  const article = button.parentElement;
-
-  // id값만 뽑아내기
-  const index = article.dataset.index.slice(5);
-
-  // handler를 비동기로 만들지 않기 때문에 promise then으로 작성
-  som.delete(`${END_POINT}/${index}`).then(() => {
-    alert('complete delete!');
+// 새로고침을 해도 textarea 입력란에 데이터 유지
+function init() {
+  getStorage('text').then((res) => {
+    text.value = res;
   });
 }
 
-userCardInner.addEventListener('click', handleDeleteCard);
+// 로컬 스토리지에 입력값 저장
+function handleInput() {
+  setStorage('text', this.value);
+}
+
+// 로컬 스토리지의 데이터 삭제
+function handleClear() {
+  text.value = '';
+  deleteStorage('text');
+}
+
+text.addEventListener('input', handleInput);
+clear.addEventListener('click', handleClear);
+
+init();
