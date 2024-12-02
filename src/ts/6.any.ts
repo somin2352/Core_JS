f318eb8ee8d51bb8f27c69bb781c93a380406017
somin2352 <somin030502@gmail.com>
2024-12-02 16:20:22 +0900

    any, unknown

---
+++ src/ts/6.any.ts
@@ -0,0 +1,18 @@
+// any
+let obj: any;
+
+obj = { x: 10 };
+obj = 123;
+obj = () => {};
+obj.toUpperCase();
+
+// unknown
+let arr: unknown;
+
+arr = 1;
+arr = "hello";
+
+// 타입 좁히기 (narrowing)
+if (typeof arr === "number") {
+  arr.toFixed();
+}
