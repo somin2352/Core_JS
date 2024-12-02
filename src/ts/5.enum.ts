bc71286e1dcfa460d7b8ac2f767557a883a3f58f
somin2352 <somin030502@gmail.com>
2024-12-02 16:19:55 +0900

    enum type

---
+++ src/ts/5.enum.ts
@@ -0,0 +1,17 @@
+// enumerable type
+// 찐상수
+enum Direction {
+  UP = 3,
+  DOWN,
+  LEFT,
+  RIGHT,
+}
+
+Direction.UP; //0
+
+const direction = {
+  up: Direction.UP,
+  down: Direction.DOWN,
+  left: Direction.LEFT,
+  right: Direction.RIGHT,
+};
