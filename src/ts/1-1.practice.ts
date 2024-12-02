5621ff3f1dc6853720a877e34a6b0cad367dfbe7
somin2352 <somin030502@gmail.com>
2024-12-02 16:17:27 +0900

    primitive type

---
+++ src/ts/1-1.practice.ts
@@ -0,0 +1,14 @@
+/* primitive type */
+
+// string
+let strP: string = "name";
+// number
+let numP: number = 52;
+// boolean
+let boolP: boolean = true;
+// null
+let nullP: null = null;
+// undefined
+let undefP: undefined = undefined;
+
+console.log(boolP);
