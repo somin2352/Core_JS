402117bdabe7d27b1696be8c65c4420098d0f627
somin2352 <somin030502@gmail.com>
2024-12-04 16:35:07 +0900

    Header

---
+++ src/Layout/Header.ts
@@ -0,0 +1,54 @@
+import { LitElement, html, css, CSSResultGroup } from "lit";
+import { customElement } from "lit/decorators.js";
+import resetCSS from "./resetCSS";
+
+@customElement("c-header")
+class Header extends LitElement {
+  static styles: CSSResultGroup = [
+    resetCSS,
+    css`
+      header {
+        display: flex;
+        justify-content: space-between;
+        background-color: white;
+        color: black;
+        padding: 1rem;
+
+        .logo {
+          display: flex;
+          align-items: center;
+          gap: 0.3rem;
+        }
+
+        nav {
+          display: flex;
+          align-items: center;
+
+          ul {
+            display: flex;
+            gap: 1rem;
+          }
+        }
+      }
+    `,
+  ];
+
+  render() {
+    return html`
+      <header>
+        <h1 class="logo">
+          <a href="/"><img src="/logo.png" alt="호랑이얼굴" style="width:30px" /></a>
+          <span>HypeCart</span>
+        </h1>
+        <nav>
+          <ul>
+            <li><a href="/">About</a></li>
+            <li><a href="/src/pages/product/">Product</a></li>
+            <li><a href="/">Contact</a></li>
+            <li><a href="/">Login</a></li>
+          </ul>
+        </nav>
+      </header>
+    `;
+  }
+}
