3c1286f65e71b97ab2fbc82a052f603ef450fea0
somin2352 <somin030502@gmail.com>
2024-12-05 17:30:54 +0900

    로그인 여부에 따른 header 변경

--- src/Layout/Header.ts
+++ src/Layout/Header.ts
@@ -1,10 +1,16 @@
-import { LitElement, html, css, CSSResultGroup } from "lit";
-import { customElement } from "lit/decorators.js";
+import { css, CSSResultGroup, html, LitElement } from "lit";
+import { customElement, state } from "lit/decorators.js";
+import Swal from "sweetalert2";
+import "sweetalert2/src/sweetalert2.scss";
+import { Auth } from "../@types/type";
 import resetCSS from "./resetCSS";
+import pb from "../api/pocketbase";
 
 // "c-header"라는 태그 이름을 가진 커스텀 엘리먼트를 등록
 @customElement("c-header")
 class Header extends LitElement {
+  @state() private loginData: Auth = {} as Auth;
+
   // LitElement의 정적 styles 속성을 사용하여 컴포넌트의 스타일 정의
   static styles: CSSResultGroup = [
     resetCSS,
@@ -35,7 +41,39 @@ class Header extends LitElement {
     `,
   ];
 
+  connectedCallback() {
+    super.connectedCallback();
+    this.fetchData();
+  }
+
+  fetchData() {
+    const auth = JSON.parse(localStorage.getItem("auth") ?? "{}");
+
+    this.loginData = auth;
+  }
+
+  handleLogout(e: Event) {
+    e.preventDefault();
+    Swal.fire({
+      title: "로그아웃",
+      text: "로그아웃 하시겠습니까?",
+      icon: "question",
+      showCancelButton: true,
+      confirmButtonText: "로그아웃",
+    }).then((isConfirmed) => {
+      if (isConfirmed) {
+        localStorage.removeItem("auth");
+        pb.authStore.clear(); //pocketbase 데이터 삭제
+        // this.loginData.isAuth = false; // 상태 변경
+        // this.requestUpdate(); //강제 업데이트
+        location.reload();
+      }
+    });
+  }
+
   render() {
+    const { isAuth, user } = this.loginData;
+
     return html`
       <header>
         <h1 class="logo">
@@ -47,7 +85,14 @@ class Header extends LitElement {
             <li><a href="/">About</a></li>
             <li><a href="/src/pages/product/">Product</a></li>
             <li><a href="/">Contact</a></li>
-            <li><a href="/">Login</a></li>
+            <li>
+              ${!isAuth
+                ? html`<a href="/src/pages/login/">Login</a>`
+                : html` <div>
+                    <span>${user.name}님</span>
+                    <a href="/" @click=${this.handleLogout}>Logout</a>
+                  </div>`}
+            </li>
           </ul>
         </nav>
       </header>
