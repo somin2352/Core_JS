5be6799e74353e251f8cf9166ea714a70a011978
somin2352 <somin030502@gmail.com>
2024-12-05 17:26:38 +0900

    npm run build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from "vite";
+import { resolve } from "node:path";
 
 export default defineConfig({
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
+  },
+  build: {
+    outDir: "docs",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        product: resolve(__dirname, "src/pages/product/index.html"),
+        login: resolve(__dirname, "src/pages/login/index.html"),
+      },
+    },
   },
 });
